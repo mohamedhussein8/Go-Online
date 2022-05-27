04624f9e7b5a7ba6a5d18543a07a0a91208ea8ab
AhmedEltohamy <ahmedtohamy759@gmail.com>
2022-05-27 16:43:42 +0200

    Handel Possible Authentication Errors from the Server

--- src/app/Components/SharedComponents/register/register.component.ts
+++ src/app/Components/SharedComponents/register/register.component.ts
@@ -12,18 +12,50 @@ export class RegisterComponent implements OnInit {
 
   public newUser: RegisterVM = new RegisterVM();
   public confimPass: string = "";
+  public errorMessage: string;
+  public serverError: boolean;
 
-  constructor(private accountService: AccountService, private router: Router) {  }
+  constructor(private accountService: AccountService, private router: Router) 
+  { 
+    this.errorMessage = "";
+    this.serverError = false;
+  }
 
   ngOnInit(): void {
   }
 
   Register()
   {
-    this.accountService.Register(this.newUser).subscribe(data => {
-      localStorage.setItem("User", JSON.stringify(data));
-      this.router.navigate(['/Home']);
-    });
-  }
+    this.errorMessage = "";
+    this.serverError = false;
 
+    this.accountService.Register(this.newUser).subscribe(
+      {
+        next: data => {
+          localStorage.setItem("User", JSON.stringify(data));
+          this.router.navigate(['/Home']);
+        },
+        error: err => {
+          this.serverError = true;
+          
+          if(typeof err["error"] == "string")
+            this.errorMessage = err["error"];
+          
+          else if(err["error"].hasOwnProperty("errors"))
+            for(const e of err["error"]["errors"])
+              this.errorMessage += e; 
+          
+          else if(err["error"].hasOwnProperty("Email"))  
+            for(const e of err["error"]["Email"])
+              this.errorMessage += e; 
+          
+          else if(err["error"].hasOwnProperty("UserName"))  
+            for(const e of err["error"]["UserName"])
+              this.errorMessage += e; 
+  
+          else
+            this.errorMessage = "Error has been occured, Please Try Again";
+        }  
+      });
+  }
 }
